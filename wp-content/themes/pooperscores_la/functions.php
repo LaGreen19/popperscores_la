2da2ff1b24c8f8e4ca1230328973cee07bc745eb
LaGreen19 <lagreen227@gmail.com>
2016-12-13 13:30:23 -0500

    menu styles added

--- wp-content/themes/pooperscores_la/functions.php
+++ wp-content/themes/pooperscores_la/functions.php
@@ -7,7 +7,7 @@
  * @package Pooperscores
  */
 
-if ( ! function_exists( 'pooperscores_la_setup' ) ) :
+if ( ! function_exists( 'popperscores_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
@@ -15,18 +15,16 @@ if ( ! function_exists( 'pooperscores_la_setup' ) ) :
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function pooperscores_la_setup() {
+function popperscores_setup() {
 	/*
 	 * Make theme available for translation.
 	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Pooperscores, use a find and replace
-	 * to change 'pooperscores_la' to the name of your theme in all the template files.
+	 * If you're building a theme based on Popperscores, use a find and replace
+	 * to change 'popperscores' to the name of your theme in all the template files.
 	 */
-	load_theme_textdomain( 'pooperscores_la', get_template_directory() . '/languages' );
-
+	load_theme_textdomain( 'popperscores', get_template_directory() . '/languages' );
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
-
 	/*
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
@@ -34,19 +32,16 @@ function pooperscores_la_setup() {
 	 * provide it for us.
 	 */
 	add_theme_support( 'title-tag' );
-
 	/*
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
-
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary', 'pooperscores_la' ),
+		'primary' => esc_html__( 'Primary Menu', 'popperscores' ),
 	) );
-
 	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
@@ -58,16 +53,25 @@ function pooperscores_la_setup() {
 		'gallery',
 		'caption',
 	) );
-
+	/*
+	 * Enable support for Post Formats.
+	 * See https://developer.wordpress.org/themes/functionality/post-formats/
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside',
+		'image',
+		'video',
+		'quote',
+		'link',
+	) );
 	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'pooperscores_la_custom_background_args', array(
+	add_theme_support( 'custom-background', apply_filters( 'popperscores_custom_background_args', array(
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
 }
-endif;
-add_action( 'after_setup_theme', 'pooperscores_la_setup' );
-
+endif; // popperscores_setup
+add_action( 'after_setup_theme', 'popperscores_setup' );
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
@@ -75,72 +79,67 @@ add_action( 'after_setup_theme', 'pooperscores_la_setup' );
  *
  * @global int $content_width
  */
-function pooperscores_la_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'pooperscores_la_content_width', 640 );
+function popperscores_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'popperscores_content_width', 640 );
 }
-add_action( 'after_setup_theme', 'pooperscores_la_content_width', 0 );
-
+add_action( 'after_setup_theme', 'popperscores_content_width', 0 );
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function pooperscores_la_widgets_init() {
+function popperscores_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'pooperscores_la' ),
+		'name'          => esc_html__( 'Sidebar', 'popperscores' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'pooperscores_la' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
 }
-add_action( 'widgets_init', 'pooperscores_la_widgets_init' );
-
+add_action( 'widgets_init', 'popperscores_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
-function pooperscores_la_scripts() {
-	wp_enqueue_style( 'pooperscores_la-style', get_stylesheet_uri() );
-        
-        
-         /*****Add Google Fonts: Fira Sans and Merriweahter*/
-        wp_enqueue_style ('pooperscores_la-google-fonts', 'https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700,700i|Merriweather:400,400i,700,700i');
-       
-        
-       
-
-	wp_enqueue_script( 'pooperscores_la-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'pooperscores_la-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
+function popperscores_scripts() {
+	wp_enqueue_style( 'popperscores-style', get_stylesheet_uri() );
+	
+	// Add Google Fonts: Fira Sans and Merriweather
+	wp_enqueue_style( 'popperscores-local-fonts', get_template_directory_uri() . '/fonts/custom-fonts.css' );
+	
+	// Add Font Awesome icons (http://fontawesome.io) 
+	wp_enqueue_style( 'popperscores-fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' );
+	
+	wp_enqueue_script( 'popperscores-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20120206', true );
+	wp_localize_script( 'popperscores-navigation', 'screenReaderText', array(
+		'expand'   => '<span class="screen-reader-text">' . __( 'expand child menu', 'popperscores' ) . '</span>',
+		'collapse' => '<span class="screen-reader-text">' . __( 'collapse child menu', 'popperscores' ) . '</span>',
+	) );
+	
+	wp_enqueue_script( 'popperscores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'pooperscores_la_scripts' );
-
+add_action( 'wp_enqueue_scripts', 'popperscores_scripts' );
 /**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
-
 /**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
-
 /**
  * Custom functions that act independently of the theme templates.
  */
 require get_template_directory() . '/inc/extras.php';
-
 /**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
 /**
  * Load Jetpack compatibility file.
  */
--- wp-content/themes/twentyfifteen/js/functions.js
+++ wp-content/themes/twentyfifteen/js/functions.js
@@ -1,19 +1,87 @@
-/* global screenReaderText */
 /**
- * Theme functions file.
+ * navigation.js
  *
- * Contains handlers for navigation and widget area.
+ * Handles toggling the navigation menu for small screens and enables tab
+ * support for dropdown menus.
  */
-
 ( function( $ ) {
-	var $body, $window, $sidebar, adminbarOffset, top = false,
-	    bottom = false, windowWidth, windowHeight, lastWindowPos = 0,
-	    topOffset = 0, bodyHeight, sidebarHeight, resizeTimer,
-	    secondary, button;
+	var container, button, menu, links, subMenus;
 
+	container = document.getElementById( 'site-navigation' );
+	if ( ! container ) {
+		return;
+	}
+
+	button = container.getElementsByTagName( 'button' )[0];
+	if ( 'undefined' === typeof button ) {
+		return;
+	}
+
+	menu = container.getElementsByTagName( 'ul' )[0];
+
+	// Hide menu toggle button if menu is empty and return early.
+	if ( 'undefined' === typeof menu ) {
+		button.style.display = 'none';
+		return;
+	}
+
+	menu.setAttribute( 'aria-expanded', 'false' );
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
+		menu.className += ' nav-menu';
+	}
+
+	button.onclick = function() {
+		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+			container.className = container.className.replace( ' toggled', '' );
+			button.setAttribute( 'aria-expanded', 'false' );
+			menu.setAttribute( 'aria-expanded', 'false' );
+		} else {
+			container.className += ' toggled';
+			button.setAttribute( 'aria-expanded', 'true' );
+			menu.setAttribute( 'aria-expanded', 'true' );
+		}
+	};
+
+	// Get all the link elements within the menu.
+	links    = menu.getElementsByTagName( 'a' );
+	subMenus = menu.getElementsByTagName( 'ul' );
+
+	// Set menu items with submenus to aria-haspopup="true".
+	for ( var i = 0, len = subMenus.length; i < len; i++ ) {
+		subMenus[i].parentNode.setAttribute( 'aria-haspopup', 'true' );
+	}
+
+	// Each time a menu link is focused or blurred, toggle focus.
+	for ( i = 0, len = links.length; i < len; i++ ) {
+		links[i].addEventListener( 'focus', toggleFocus, true );
+		links[i].addEventListener( 'blur', toggleFocus, true );
+	}
+
+	/**
+	 * Sets or removes .focus class on an element.
+	 */
+	function toggleFocus() {
+		var self = this;
+
+		// Move up through the ancestors of the current link until we hit .nav-menu.
+		while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
+
+			// On li elements toggle the class .focus.
+			if ( 'li' === self.tagName.toLowerCase() ) {
+				if ( -1 !== self.className.indexOf( 'focus' ) ) {
+					self.className = self.className.replace( ' focus', '' );
+				} else {
+					self.className += ' focus';
+				}
+			}
+
+			self = self.parentElement;
+		}
+	}
+	
 	function initMainNavigation( container ) {
 		// Add dropdown toggle that display child menu items.
-		container.find( '.menu-item-has-children > a' ).after( '<button class="dropdown-toggle" aria-expanded="false">' + screenReaderText.expand + '</button>' );
+		container.find( '.menu-item-has-children > a, .page_item_has_children > a' ).after( '<button class="dropdown-toggle" aria-expanded="false">' + screenReaderText.expand + '</button>' );
 
 		// Toggle buttons and submenu items with active children menu items.
 		container.find( '.current-menu-ancestor > button' ).addClass( 'toggle-on' );
@@ -42,137 +110,4 @@
 			});
 		}
 	});
-
-	secondary = $( '#secondary' );
-	button = $( '.site-branding' ).find( '.secondary-toggle' );
-
-	// Enable menu toggle for small screens.
-	( function() {
-		var menu, widgets, social;
-		if ( ! secondary.length || ! button.length ) {
-			return;
-		}
-
-		// Hide button if there are no widgets and the menus are missing or empty.
-		menu    = secondary.find( '.nav-menu' );
-		widgets = secondary.find( '#widget-area' );
-		social  = secondary.find( '#social-navigation' );
-		if ( ! widgets.length && ! social.length && ( ! menu.length || ! menu.children().length ) ) {
-			button.hide();
-			return;
-		}
-
-		button.on( 'click.twentyfifteen', function() {
-			secondary.toggleClass( 'toggled-on' );
-			secondary.trigger( 'resize' );
-			$( this ).toggleClass( 'toggled-on' );
-			if ( $( this, secondary ).hasClass( 'toggled-on' ) ) {
-				$( this ).attr( 'aria-expanded', 'true' );
-				secondary.attr( 'aria-expanded', 'true' );
-			} else {
-				$( this ).attr( 'aria-expanded', 'false' );
-				secondary.attr( 'aria-expanded', 'false' );
-			}
-		} );
-	} )();
-
-	/**
-	 * @summary Add or remove ARIA attributes.
-	 * Uses jQuery's width() function to determine the size of the window and add
-	 * the default ARIA attributes for the menu toggle if it's visible.
-	 * @since Twenty Fifteen 1.1
-	 */
-	function onResizeARIA() {
-		if ( 955 > $window.width() ) {
-			button.attr( 'aria-expanded', 'false' );
-			secondary.attr( 'aria-expanded', 'false' );
-			button.attr( 'aria-controls', 'secondary' );
-		} else {
-			button.removeAttr( 'aria-expanded' );
-			secondary.removeAttr( 'aria-expanded' );
-			button.removeAttr( 'aria-controls' );
-		}
-	}
-
-	// Sidebar scrolling.
-	function resize() {
-		windowWidth = $window.width();
-
-		if ( 955 > windowWidth ) {
-			top = bottom = false;
-			$sidebar.removeAttr( 'style' );
-		}
-	}
-
-	function scroll() {
-		var windowPos = $window.scrollTop();
-
-		if ( 955 > windowWidth ) {
-			return;
-		}
-
-		sidebarHeight = $sidebar.height();
-		windowHeight  = $window.height();
-		bodyHeight    = $body.height();
-
-		if ( sidebarHeight + adminbarOffset > windowHeight ) {
-			if ( windowPos > lastWindowPos ) {
-				if ( top ) {
-					top = false;
-					topOffset = ( $sidebar.offset().top > 0 ) ? $sidebar.offset().top - adminbarOffset : 0;
-					$sidebar.attr( 'style', 'top: ' + topOffset + 'px;' );
-				} else if ( ! bottom && windowPos + windowHeight > sidebarHeight + $sidebar.offset().top && sidebarHeight + adminbarOffset < bodyHeight ) {
-					bottom = true;
-					$sidebar.attr( 'style', 'position: fixed; bottom: 0;' );
-				}
-			} else if ( windowPos < lastWindowPos ) {
-				if ( bottom ) {
-					bottom = false;
-					topOffset = ( $sidebar.offset().top > 0 ) ? $sidebar.offset().top - adminbarOffset : 0;
-					$sidebar.attr( 'style', 'top: ' + topOffset + 'px;' );
-				} else if ( ! top && windowPos + adminbarOffset < $sidebar.offset().top ) {
-					top = true;
-					$sidebar.attr( 'style', 'position: fixed;' );
-				}
-			} else {
-				top = bottom = false;
-				topOffset = ( $sidebar.offset().top > 0 ) ? $sidebar.offset().top - adminbarOffset : 0;
-				$sidebar.attr( 'style', 'top: ' + topOffset + 'px;' );
-			}
-		} else if ( ! top ) {
-			top = true;
-			$sidebar.attr( 'style', 'position: fixed;' );
-		}
-
-		lastWindowPos = windowPos;
-	}
-
-	function resizeAndScroll() {
-		resize();
-		scroll();
-	}
-
-	$( document ).ready( function() {
-		$body          = $( document.body );
-		$window        = $( window );
-		$sidebar       = $( '#sidebar' ).first();
-		adminbarOffset = $body.is( '.admin-bar' ) ? $( '#wpadminbar' ).height() : 0;
-
-		$window
-			.on( 'scroll.twentyfifteen', scroll )
-			.on( 'load.twentyfifteen', onResizeARIA )
-			.on( 'resize.twentyfifteen', function() {
-				clearTimeout( resizeTimer );
-				resizeTimer = setTimeout( resizeAndScroll, 500 );
-				onResizeARIA();
-			} );
-		$sidebar.on( 'click.twentyfifteen keydown.twentyfifteen', 'button', resizeAndScroll );
-
-		resizeAndScroll();
-
-		for ( var i = 1; i < 6; i++ ) {
-			setTimeout( resizeAndScroll, 100 * i );
-		}
-	} );
-
 } )( jQuery );
