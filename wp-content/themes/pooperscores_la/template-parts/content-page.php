a49d31f8e8c883b723d5f2348d5b97c2db3b23b5
LaGreen19 <lagreen227@gmail.com>
2016-12-15 09:47:29 -0500

    archive pages

--- wp-content/themes/pooperscores_la/template-parts/content-page.php
+++ wp-content/themes/pooperscores_la/template-parts/content-page.php
@@ -4,40 +4,47 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package Pooperscores
+ * @package Popperscores
  */
 
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		
+		<?php 
+		if ( has_post_thumbnail() ) { ?>
+			<figure class="featured-image">
+				<?php the_post_thumbnail(); ?>
+			</figure>
+		<?php }
+		?>
+		
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+		<?php the_content(); ?>
 		<?php
-			the_content();
-
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pooperscores_la' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'popperscores' ),
 				'after'  => '</div>',
 			) );
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						/* translators: %s: Name of current post */
-						esc_html__( 'Edit %s', 'pooperscores_la' ),
-						the_title( '<span class="screen-reader-text">"', '"</span>', false )
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+	<footer class="entry-footer">
+		<?php
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					esc_html__( 'Edit %s', 'popperscores' ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+		?>
+	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
+
